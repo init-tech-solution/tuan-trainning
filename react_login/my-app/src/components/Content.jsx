b9dd54a6876aa4361ff8b616c7eebf6661f88f7e
KeycoolrGb <73753209+KeycoolrGb@users.noreply.github.com>
2021-01-05 23:11:28 +0700

    Update Content.jsx
    .

--- react_login/my-app/src/components/Content.jsx
+++ react_login/my-app/src/components/Content.jsx
@@ -3,27 +3,6 @@ import "./style.css";
 
 export default function Content() {
   return (
-    // <div className="login-page">
-    //  <div className="form">
-    //   <form className="register-form">
-    //     <input type="text" placeholder="name" />
-    //     <input type="password" placeholder="password" />
-    //     <input type="text" placeholder="email address" />
-    //     <button>create</button>
-    //     <p className="message">
-    //       Already registered? <a href="#">Sign In</a>
-    //     </p>
-    //   </form>
-    //   <form className="login-form">
-    //     <input type="text" placeholder="username" />
-    //     <input type="password" placeholder="password" />
-    //     <button>login</button>
-    //     <p className="message">
-    //       Not registered? <a href="#">Create an account</a>
-    //     </p>
-    //  </form>
-    //  </div>
-    // </div>
     <div className="login-page">
       <div className="form">
         <h1 className="title">WELCOME TO INIT</h1>
