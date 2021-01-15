016d8723d2aadb6d572739090243954e5e8f3c7d
minhnq0702 <minhnq.0702@gmail.com>
2021-01-15 14:23:27 +0700

    fix bug

--- react_login/my-app/src/components/ProtectedRoute/index.jsx
+++ react_login/my-app/src/components/ProtectedRoute/index.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
-import { useHistory, Redirect } from "react-router-dom";
+import { useHistory, Redirect, Route } from "react-router-dom";
 
 export default function ProtectedRoute(props) {
   // if (props.renderComp) {
   //   return localStorage.getItem('token') ? props.renderComp()
   // : <Redirect to={props.fail} />;
   // }
+  let isLogin = localStorage.getItem("token")
+  console.log("Is login?===>", isLogin)
 
-  return localStorage.getItem("token") ? (
-    <Redirect to={props.success} />
+  return isLogin ? (
+    // <Redirect to={props.success} />
+    <Route path={props.success} component={props.component}/>
   ) : (
     <Redirect to={props.fail} />
   );
